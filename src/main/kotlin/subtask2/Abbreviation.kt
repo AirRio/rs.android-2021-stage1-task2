7fe5b90d57327db3773a14a170a7fc8e29433433
AirRio <45617410+AirRio@users.noreply.github.com>
2021-06-10 15:46:57 +0300

    Update Abbreviation.kt

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -4,10 +4,21 @@ class Abbreviation {
     
     fun abbreviationFromA(a: String, b: String): String {
 
-        val string = a.toUpperCase()
-        val result = string.substring(b.length-1)
+        var string = a.toUpperCase()
+        var pp = ""
 
-        return if(result.length == b.length)
+        for(arg in string)
+        {
+            for(p in b)
+            {
+                if(arg == p)
+                {
+                    pp += arg
+                }
+            }
+        }
+
+        return if(pp == b)
             "YES"
         else "NO"
     }
